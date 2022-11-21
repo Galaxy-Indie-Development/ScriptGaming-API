c0a061ad51f7dcd02c19fc40841dcd86c611b6b2
BigTallahasee <85016240+BigTallahasee@users.noreply.github.com>
2022-11-21 17:48:44 -0600

    Update sga.js

--- sga.js
+++ sga.js
@@ -4,8 +4,7 @@ export class SGA{
     constructor(){
         this.graphics = [
             graphics.rectangle(color, x, y, width, height),
-            graphics.text(string, color, x, y)]
+            graphics.text(string, color, x, y)
         ]
-        
     }
 }
